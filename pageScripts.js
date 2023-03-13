3ff46b5d2352ff9eb96c17cc12946fab35fe7d18
Royalo04 <filip.sankowski@gmail.com>
2023-03-13 21:37:21 +0100

    candidates are saved to variable on page

--- pageScripts.js
+++ pageScripts.js
@@ -1,13 +1,26 @@
+votePageScript();
+
+// ==================
+async function votePageScript() {
+  const candidates = await getCandidates();
+  console.log(candidates);
+}
+
 async function getCandidates() {
   const url = 'http://127.0.0.1:3000/selectAll';
+  let results;
 
   try {
     await fetch(url)
-      .then((result) => result.json())
-      .then((result) => console.log(result));
+      .then((res) => res.json())
+      .then((res) => {
+        //console.log(res);
+        results = res;
+      });
   } catch (error) {
     throw error;
   }
-}
 
-getCandidates();
+  //console.log(results);
+  return results;
+};
